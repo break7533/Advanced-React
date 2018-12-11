07ac0f392498e3a5680bba4b3a232ac6b2b65846
break7533 <ll61295@gmail.com>
2018-12-11 23:45:03 +0000

    Lesson 20 - Deleting Items and Refreshing Cache

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
 
 export default class Item extends React.Component {
 	static propTypes = {
@@ -45,7 +46,7 @@ export default class Item extends React.Component {
 						<a>Edit ✏️</a>
 					</Link>
 					<button>Add To Cart</button>
-					<button>Delete</button>
+					<DeleteItem id={item.id}>Delete This Item</DeleteItem>
 				</div>
 			</ItemStyles>
 		);
