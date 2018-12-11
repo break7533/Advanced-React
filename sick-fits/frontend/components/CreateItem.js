a290430adafada074a39a9f48af63d69121eb1d0
break7533 <ll61295@gmail.com>
2018-12-11 21:51:47 +0000

    Lesson 18 - Upload Images

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -35,6 +35,25 @@ class CreateItem extends React.Component {
 		this.setState({ [name]: val });
 	};
 
+	uploadFile = async (e) => {
+		const files = e.target.files;
+		const data = new FormData();
+		data.append('file', files[0]);
+		data.append('upload_preset', 'sickfits');
+
+		const res = await fetch('https://api.cloudinary.com/v1_1/break7533/image/upload', {
+			method: 'POST',
+			body: data
+		});
+
+		const file = await res.json();
+		console.log(file);
+		this.setState({
+			image: file.secure_url,
+			largeImage: file.eager[0].secure_url
+		});
+	};
+
 	render() {
 		return (
 			<Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -51,6 +70,19 @@ class CreateItem extends React.Component {
 					>
 						<Error error={error} />
 						<fieldset disabled={loading} aria-busy={loading}>
+							<label htmlFor="file">
+								Image
+								<input
+									type="file"
+									id="file"
+									name="file"
+									placeholder="Upload An Image"
+									required
+									onChange={this.uploadFile}
+								/>
+								{this.state.image && <img src={this.state.image} width="200px" alt="Upload Preview" />}
+							</label>
+
 							<label htmlFor="title">
 								Title
 								<input
