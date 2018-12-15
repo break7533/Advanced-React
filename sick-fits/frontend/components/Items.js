79a6f9b84b0fa5e72ad28cbb060ca272a88d49ac
break7533 <ll61295@gmail.com>
2018-12-15 12:54:12 +0000

    Lesson 23 - Pagination filters

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import style from 'styled-components';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
+	query ALL_ITEMS_QUERY($skip: Int = 0,$first: Int = ${perPage}) {
+		items(first: $first,skip:$skip,orderBy:createdAt_DESC) {
 			id
 			title
 			price
@@ -31,8 +31,14 @@ class Items extends React.Component {
 	render() {
 		return (
 			<Center>
-				<Pagination page={this.props.page}/>
-				<Query query={ALL_ITEMS_QUERY}>
+				<Pagination page={this.props.page} />
+				<Query
+					query={ALL_ITEMS_QUERY}
+					// fetchPolicy="network-only"
+					variables={{
+						skip: this.props.page * perPage - perPage
+					}}
+				>
 					{({ data, error, loading }) => {
 						if (loading) return <p>Loading...</p>;
 						if (error) return <p>Error: {error.message}</p>;
