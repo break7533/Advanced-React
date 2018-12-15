0e094463bccc7f7dde279d4ad2cbb85a2ce09395
break7533 <ll61295@gmail.com>
2018-12-15 15:33:54 +0000

    Lesson 24 - Signup of a User

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,15 +1,18 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
+server.express.use(cookieParser());
+
 server.start(
 	{
 		cors: {
 			credentials: true,
 			origin: process.env.FRONTEND_URL
-		},
+		}
 	},
 	(deets) => {
 		console.log(`Server is now running on port http://localhost:${deets.port}`);
