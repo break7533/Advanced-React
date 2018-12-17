bd04e6248b8efab83fb54679569cde35df68421d
Luís Lopes <ll61295@gmail.com>
2018-12-17 20:48:04 +0000

    Lesson 40 - ServerSide Add to Cart

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -187,6 +187,49 @@ const Mutations = {
 			},
 			info
 		);
+	},
+	async addToCart(parent, args, ctx, info) {
+		const { userId } = ctx.request;
+
+		if (!userId) {
+			throw new Error('You must ne logged in to do that!');
+		}
+		const [ existingCartItem ] = await ctx.db.query.cartItems({
+			where: {
+				user: { id: userId },
+				item: { id: args.id }
+			}
+		});
+
+		if (existingCartItem) {
+			return ctx.db.mutation.updateCartItem(
+				{
+					where: {
+						id: existingCartItem.id
+					},
+					data: {
+						quantity: existingCartItem.quantity + 1
+					}
+				},
+				info
+			);
+		}
+
+		return ctx.db.mutation.createCartItem(
+			{
+				data: {
+					user: {
+						connect: { id: userId }
+					},
+					item: {
+						connect: {
+							id: args.id
+						}
+					}
+				}
+			},
+			info
+		);
 	}
 };
 
