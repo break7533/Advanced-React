45a79e36932f1128e8e64fba232b26f7101957df
Luís Lopes <ll61295@gmail.com>
2018-12-27 16:17:16 +0000

    Lesson 51 - Saving Order in Database

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -278,6 +278,7 @@ const Mutations = {
 						id 
 						description 
 						image
+						largeImage
 					}
 				}
 			}`
@@ -290,7 +291,34 @@ const Mutations = {
 			currency: 'USD',
 			source: args.token
 		});
-		
+
+		const orderItems = user.cart.map((cartItem) => {
+			const orderItem = {
+				...cartItem.item,
+				quantity: cartItem.quantity,
+				user: { connect: { id: userId } }
+			};
+			delete orderItem.id;
+			return orderItem;
+		});
+
+		const order = await ctx.db.mutation.createOrder({
+			data: {
+				total: charge.amount,
+				charge: charge.id,
+				items: { create: orderItems },
+				user: { connect: { id: userId } }
+			}
+		});
+
+		const cartItemIds = user.cart.map((cartItem) => cartItem.id);
+		await ctx.db.mutation.deleteManyCartItems({
+			where: {
+				id_in: cartItemIds
+			}
+		});
+
+		return order;
 	}
 };
 
