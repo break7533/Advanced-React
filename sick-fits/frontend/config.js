0079d550653971fb6a363fc98ff748981202952e
Luís Lopes <ll61295@gmail.com>
2019-01-03 17:06:44 +0000

    Lesson 68 - Prod Configs for deploy

--- sick-fits/frontend/config.js
+++ sick-fits/frontend/config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://sickfits-yoga-breakprod.herokuapp.com/`;
 export const perPage = 4;
-export const stripeKey = "pk_test_jtwrFvfnZviIemqpYU08IuqT";
+export const stripeKey = 'pk_test_jtwrFvfnZviIemqpYU08IuqT';
